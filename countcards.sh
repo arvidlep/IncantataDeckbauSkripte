cec7b7e36091652e33fdbcf03837e1c419725d5b
Arvid Lepsien <arvid.lepsien@gmail.com>
2017-11-24 05:36:39 +0100

    Fixed countcards.sh to use ; as delimiter and corrected the regex

--- countcards.sh
+++ countcards.sh
@@ -1,2 +1,2 @@
 #Greppt jede relevante Karte und addiert die Anzahlen
-grep -E "9|[1-9][0-9]|[1-9][0-9][0-0]" $1 | awk -F"," '{x+=$2}END{print "Anzahl Karten im Deck: " x}'
+grep -E "9|[1-9][0-9]+" $1 | awk -F";" '{x+=$2}END{print "Anzahl Karten im Deck: " x}'
