54e0a6e96ddfc82c18855e52cb17e1b4824594cc
Arvid Lepsien <arvid.lepsien@gmail.com>
2017-11-24 05:07:04 +0100

    Updated comment in icdeckToCSV.sh

--- icdeckToCSV.sh
+++ icdeckToCSV.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#Entfernt allen horizontalen whitespace und alles was in einer Zeile nach // steht
+#Entfernt allen whitespace und alles was in einer Zeile nach // steht
 sed -e 's|//.*||g' $1 | tr -d "[:blank:]" | sed '/^$/d' > $2
